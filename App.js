57360f703e966c06f1dce6f70592da6c82d627d2
jfsene <jfsene@stefanini.com>
2017-11-02 11:33:18 -0200

    commit before installing library

--- App.js
+++ App.js
@@ -5,25 +5,22 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev men' +
+      'u'
 });
 
-export default class App extends Component<{}> {
-  render() {
+export default class App extends Component < {} > {
+  render( ) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Some Text</Text>
+      <View>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
       </View>
     );
   }
@@ -33,17 +30,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
